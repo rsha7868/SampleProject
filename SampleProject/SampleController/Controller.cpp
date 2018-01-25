0c75dc6b70f84701b438b7b7fea7fb0f74f9adec
Sharp <rsha7868@740s-pl-127.local>
2018-01-25 12:56:55 -0700

    SAve...

--- SampleProject/SampleController/Controller.cpp
+++ SampleProject/SampleController/Controller.cpp
@@ -20,24 +20,24 @@ void Controller :: start()
     int i = 1;
     int a = 75;
     int o = 0;
-    
+
     for(int i = o; i < a; i++)
     {
         cout << "This is the " << i << " time in a loop" << endl;
     }
 }
-void Controller :: start()
-{
-    int a = 15;
-    int b = 45;
-    int c = 50;
-    double = y;
-    
-    for(a + b + c = y)
-    {
-        cout << "The anwser is " << y << endl;
-    }
-}
+//void Controller :: start()
+//{
+//    int a = 15;
+//    int b = 45;
+//    int c = 50;
+//    int total = y;
+//
+//    if(a + b + c = y)
+//    {
+//        cout << "The anwser is " << y << endl;
+//    }
+//}
 
 
 
