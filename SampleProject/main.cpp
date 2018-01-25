5ac69a6003f19f0425da079b7e1a64ad47e2f9c5
Sharp <rsha7868@740s-pl-127.local>
2018-01-25 11:47:24 -0700

    Initial commit

---
+++ SampleProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SampleProject
+//
+//  Created by Sharp, Ryan on 1/25/18.
+//  Copyright © 2018 Sharp, Ryan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
