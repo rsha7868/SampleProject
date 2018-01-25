09f22b166e155d487f9dc8a4a4880f553ce3c184
Sharp <rsha7868@740s-pl-127.local>
2018-01-25 11:52:09 -0700

    made a controller and a runner in Sample Project.

---
+++ SampleProject/SampleController/Controller.hpp
@@ -0,0 +1,14 @@
+//
+//  Controller.hpp
+//  SampleProject
+//
+//  Created by Sharp, Ryan on 1/25/18.
+//  Copyright © 2018 Sharp, Ryan. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <stdio.h>
+
+#endif /* Controller_hpp */
---
+++ SampleProject/SampleController/Runner.cpp
@@ -0,0 +1,9 @@
+//
+//  Runner.cpp
+//  SampleProject
+//
+//  Created by Sharp, Ryan on 1/25/18.
+//  Copyright © 2018 Sharp, Ryan. All rights reserved.
+//
+
+#include <stdio.h>
