f921f49216a4f16e485759c8181fb8a35b38f26c
Sharp <rsha7868@740s-pl-127.local>
2018-01-29 13:11:03 -0700

    The vector goes on forever.

--- SampleProject/SampleController/Controller.cpp
+++ SampleProject/SampleController/Controller.cpp
@@ -5,6 +5,7 @@
 //  Created by Sharp, Ryan on 1/25/18.
 //  Copyright © 2018 Sharp, Ryan. All rights reserved.
 //
+#include <iostream>
 #include <vector>
 #include "Controller.hpp"
 
@@ -17,7 +18,7 @@ Controller :: Controller()
 
 void Controller :: start()
 {
-    int i = 1;
+    int i;
     int a = 1;
     int o = 1;
 
@@ -25,12 +26,6 @@ void Controller :: start()
     {
         cout << "This is the " << i << " time in a loop" << endl;
     }
-}
-vector <int> gl;
-for(int a = 1; a <= 10; a++)
-    gl.push_size (5);
-{
-    return gl;
 }
 
 
--- SampleProject/SampleController/Runner.cpp
+++ SampleProject/SampleController/Runner.cpp
@@ -7,9 +7,46 @@
 //
 // only can relate to a .hpp not a .cpp
 #include "Controller.hpp"
+#include <iostream>
+#include <vector>
+using namespace std;
 
 int main()
 {
+    // Example
+    
+    // Create a vector to store int
+    vector<int> vec;
+    int i = 1;
+    
+    // Display the original size of vec
+    cout << "vector size = " << vec.size() << endl;
+    
+    // Access (#) of value into the vector
+    for(i = 0; i < 5; i++)
+    {
+        vec.push_back(i);
+    }
+    // Display extended size of vec
+    cout << "extended vector size = " << vec.size() << endl;
+    
+    // Access (#) values from the vector
+    for(i = 0; i < 5; i++)
+    {
+        cout << "value of vec [" << i << "] = " << vec[i] << endl;
+    }
+    
+    // Use interator to access the values
+//    vector<int> :: iterator v = vec.begin();
+//    while(v != vec.end())
+//    {
+//        cout << "value of v = " << *v << endl;
+//    }
+    
+    
+    
+    
     Controller app;
     app.start();
+    
 }
